d7ce7a747bef227984d856e09c5228cf26de9ecd
globalaplication <32783197+globalaplication@users.noreply.github.com>
2017-10-26 15:41:48 +0300

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ for test in range(1, 1000):
     msql.execute('INSERT INTO test ROW (category, TEST) NOT (TEST)', 'beta', test)
 msql.update()
 t1 = time.time()
-print '{} saniyede {} data kaydedildi.'.format(t1-t0, 5)
+print '{} saniyede {} data kaydedildi.'.format(t1-t0, 999)
 
 #msql.UPDATE_(1, 'test', 'category', 'beta')
 #msql.DELETE_(1, 'test')
